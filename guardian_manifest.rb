337692ad440d7984a193f8a32dc71d7aa5702e09
Katherine Lynch <katherly@upenn.edu>
2018-07-19 11:43:13 -0400

    WIP, successful output of XLSX manifest

--- guardian_manifest.rb
+++ guardian_manifest.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'rubyXL'
+require 'yaml'
+
+require 'pry'
 
 HEADERS = %w[todo_base source workspace compressed_destination glacier_description glacier_vault application method]
 
@@ -8,18 +11,21 @@ def missing_args?
   return ARGV[0].nil?
 end
 
-def parse_manifest(manifest)
+def parse_inventory(yml)
   inventory = []
-  manifest_lines = File.readlines(manifest)
-  manifest_lines.shift
-  manifest_lines.each do |line|
-    line_entries = {}
-    line.chomp!
-    values = line.split('|')
-    values.each_with_index do |value, position|
-      line_entries[HEADERS[position]] = value
-    end
-    inventory << line_entries
+  yaml = YAML.load_file(yml)
+  yaml['directive_names'].each do |dirname|
+    dir_entry = {}
+    dir_entry['todo_base'] = dirname
+    yaml['description_values']['description'] = dirname
+    dir_entry['source'] = "/#{yaml['source']}/#{dirname}.git"
+    dir_entry['workspace'] = "/#{yaml['workspace']}"
+    dir_entry['compressed_destination'] = "#{yaml['compressed_destination']}/#{dirname}.#{yaml['compressed_extension']}"
+    dir_entry['glacier_description'] = yaml['description_values'].to_s
+    dir_entry['glacier_vault'] = yaml['vault']
+    dir_entry['application'] = yaml['application']
+    dir_entry['method'] = yaml['method']
+    inventory << dir_entry
   end
   return inventory
 end
@@ -53,7 +59,7 @@ abort('Specify a path to a text manifest') if missing_args?
 manifest_inventory = ARGV[0]
 spreadsheet_name = ARGV[1].nil? ? 'guardian_manifest.xlsx' : "#{File.basename(ARGV[1], '.*')}.xlsx"
 workbook.set_up_spreadsheet
-inventory = parse_manifest(manifest_inventory)
+inventory = parse_inventory(manifest_inventory)
 workbook.populate(inventory)
 workbook.write(spreadsheet_name)
 puts "Spreadsheet written to #{spreadsheet_name}."
